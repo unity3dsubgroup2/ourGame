b7de2fb49fc181f63138b1af277a12f0f1243e0d
Dmitriy <temiron@gmail.com>
2015-05-24 15:52:40 +0300

    Create door functions

---
+++ Assets/DoorLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorLight : MonoBehaviour {
+	public GameObject dlight;
+	public AudioSource dsound;
+	float emission;
+	float emission1;
+	// Use this for initialization
+	void Start () {	
+	
+		}
+	// Update is called once per frame
+	void Update () {
+
+		if (gameObject.GetComponent<BoxCollider> ().enabled == false) {
+			gameObject.GetComponent<MeshRenderer> ().materials [0].SetColor ("_EmissionColor", new Color (emission, emission, emission));
+			emission = Mathf.PingPong (Time.time, 1.0f);
+			dlight.GetComponent<Light>().color = new Color (emission1,emission1,emission1);
+			dsound.enabled = false;
+		}else if(gameObject.GetComponent<BoxCollider> ().enabled == true){
+			emission1 = Mathf.PingPong (Time.time, 1.0f);
+			dlight.GetComponent<Light>().color = new Color (emission1,0,0);
+			gameObject.GetComponent<MeshRenderer> ().materials [0].SetColor ("_EmissionColor", new Color (255, 0, 0));
+			dsound.enabled = true;
+		}
+	}
+}
---
+++ Assets/Terminal.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal : MonoBehaviour {
+	public GameObject Door;
+	public AudioSource tdoor;
+	bool OnCol = false;
+	bool OffOn = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space) && OnCol == true && OffOn) {			
+			Door.GetComponent<BoxCollider>().enabled = false;
+			tdoor.Play();
+			print ("door is opened");
+		}
+		if (Input.GetKeyUp (KeyCode.Space) && OnCol) {			
+			OffOn = !OffOn;
+			print (OffOn);
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && OnCol && !OffOn) {
+			Door.GetComponent<BoxCollider> ().enabled = true;
+			tdoor.Play();
+			print ("door is closed");
+
+		}
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Enemy") {
+			OnCol = true;
+			print ("col on");
+		}
+	}
+	void OnTriggerExit(Collider other){
+		OnCol = false;		
+		print ("col off");		
+	}
+}
