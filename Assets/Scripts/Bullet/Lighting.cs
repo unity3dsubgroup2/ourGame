32d1c0b9c469b76a7da3613e566e4314b16f6efc
Petro Ostapovets <postapovets@gmail.com>
2015-05-30 22:22:06 +0300

    The BAKE have lighting now

---
+++ Assets/Scripts/Bullet/Lighting.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lighting : MonoBehaviour
+{
+	
+	public GameObject lightingReceiver;
+	public int sections = 30;
+	public int lines = 3;
+	public float lineWidth = 0.3f;
+	public float jitter = 0.1f;
+	public Material lightningMaterial;
+	public float timeToLife = 1f;
+	public float amount = 20f;
+	public GameObject owner;
+	LineRenderer[] lineRenderers = null;
+	
+	void Start ()
+	{
+		InitializeLighting ();
+		if (lightingReceiver.tag == "Player") {
+			lightingReceiver.GetComponent<PlayerHealth> ().TakeDamage (amount);
+		}
+		Destroy (gameObject, timeToLife);
+	}
+	
+	public void InitializeLighting ()
+	{
+		//Clean existing line renderers
+		lineRenderers = gameObject.GetComponentsInChildren<LineRenderer> ();
+		if (lineRenderers != null) {
+			for (int i = 0; i < lineRenderers.Length; i++)
+				DestroyImmediate (lineRenderers [i].gameObject);
+			
+			lineRenderers = null;
+		}
+		//Create game object for each line renderer and parent it to this game object
+		for (int i = 0; i < (lines); i++) {
+			GameObject temp = new GameObject ();
+			temp.transform.parent = gameObject.transform;
+			temp.name = "Line Renderer " + (i + 1);
+			temp.AddComponent<LineRenderer> ();
+		}
+		lineRenderers = gameObject.GetComponentsInChildren<LineRenderer> ();
+		//Set initial settings for each renderer
+		for (int i = 0; i < lineRenderers.Length; i++) {
+			lineRenderers [i].castShadows = false;
+			lineRenderers [i].receiveShadows = false;
+			lineRenderers [i].material = lightningMaterial;
+			
+			lineRenderers [i].SetVertexCount (sections);
+			lineRenderers [i].SetWidth (lineWidth, lineWidth);
+		}
+	}
+	
+	void Update ()
+	{
+		//Determine the length of a section of the bolt
+		Vector3 sectionVector = (lightingReceiver.transform.position - transform.position) / sections;
+		
+		//Initialise an array of vectors for the bolt
+		Vector3[] lineVectors = new Vector3[sections];
+		
+		//Calculate the vectors for the middle sections
+		for (int j = 1; j < lineVectors.Length -1; j++)
+			lineVectors [j] = transform.position + (sectionVector * j);
+		
+		//Set the values in the line renderer for ecah bolt
+		for (int j = 0; j < lines; j++) {
+			if (lineRenderers [j]) {				
+				//Set the beginning and end
+				lineRenderers [j].SetPosition (0, transform.position);
+				lineRenderers [j].SetPosition (lineVectors.Length - 1, lightingReceiver.transform.position);
+				lineRenderers [j].SetWidth (lineWidth, lineWidth);
+				
+				//Set vectors for the rest of the sections adding jitter
+				for (int k = 1; k < (sections - 1); k++)
+					lineRenderers [j].SetPosition (k, AddVectorJitter (lineVectors [k], jitter));
+			}
+		}
+	}
+	
+	//Add random jitter to vector
+	Vector3 AddVectorJitter (Vector3 vector, float jitter)
+	{
+		vector += Vector3.left * Random.Range (-jitter, jitter);
+		vector += Vector3.up * Random.Range (-jitter, jitter);
+		vector += Vector3.forward * Random.Range (-jitter, jitter);
+		
+		return vector;
+	}
+}
---
+++ Assets/Scripts/Enemy/BakeAI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BakeAI : MonoBehaviour
+{
+	public GameObject bullet;
+	public GameObject explosion;
+
+	private Transform player;
+	private Animator myAnim;
+	private NavMeshAgent navMeshAgent;
+	private EnemyHealth myHealth;
+	private float speed = 3f;
+	private Transform weapon;
+	private bool isActive = true;
+	
+	private float shotTimer = 0;
+	
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		weapon = transform.Find ("Weapon").transform;
+		myAnim = GetComponent<Animator> ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		myHealth = GetComponent<EnemyHealth> ();
+	}
+	
+	void Update ()
+	{
+		if (isActive) {
+			if (myHealth.isAlive) {
+				navMeshAgent.speed = speed;
+		
+				float angle = Vector3.Angle (transform.forward, (player.position - transform.position));
+				if (angle < 15f && Vector3.Distance (transform.position, player.position) < myHealth.distanceToShot) {
+					if (shotTimer > myHealth.shotRate && myHealth.isAlive) {
+						Shot ();
+						shotTimer = 0;
+					}
+				}
+				shotTimer += Time.deltaTime;
+				if (Vector3.Distance (transform.position, player.position) < myHealth.distanceToShot) {
+					navMeshAgent.SetDestination (player.position);
+				}
+			} else {
+				GameObject objExplosion = (GameObject)Instantiate (
+					explosion, new Vector3 (transform.position.x, 1.5f, transform.position.z), Quaternion.identity);
+				Destroy (objExplosion, 1f);
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void Shot ()
+	{
+		GameObject shot = (GameObject)Instantiate (bullet, weapon.position, Quaternion.identity);
+		shot.GetComponent<Lighting> ().amount = myHealth.damage;
+		shot.GetComponent<Lighting> ().owner = gameObject;
+		shot.GetComponent<Lighting> ().lightingReceiver = player.gameObject;
+	}
+
+
+}
